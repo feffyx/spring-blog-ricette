565592ef94493f99823137d21c65c27b81ffeaa9
feffyx <ziacofederica@gmail.com>
2024-01-22 10:25:24 +0100

    Initial commit

---
+++ src/test/java/org/test/springblogricette/SpringBlogRicetteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.test.springblogricette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBlogRicetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
